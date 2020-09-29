e9d0f739e846ade772c01565e19d2c16ba50f0d4
Puja <pujayadav@gmail.com>
2020-09-29 20:17:23 +0530

    [puja] add . present or absent

--- employeewage/Program.cs
+++ employeewage/Program.cs
@@ -6,7 +6,19 @@ namespace employeewage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to employeewages");
+           // Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
         }
     }
 }
