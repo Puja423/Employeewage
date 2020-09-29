1fa30fb28ee28cd7cf1e8106a1df2045294f6285
Puja <pujayadav@gmail.com>
2020-09-29 20:08:32 +0530

    employeewages

---
+++ employeewage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace employeewage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employeewages");
+        }
+    }
+}
