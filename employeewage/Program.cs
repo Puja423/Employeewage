865b0539cd0f2a80ddf3e3d732fd716355adfde2
Puja <pujayadav@gmail.com>
2020-10-03 13:27:14 +0530

    [puja] refractor manage employeewagw for multiple companies

--- employeewage/Program.cs
+++ employeewage/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace employeewage
+namespace EmployeesWageProgram
 {
     class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to employeewages");
+            Console.WriteLine("################# Welcome to Employee Wage System ###########");
+
+            var wageBuilder = new EmployeeWageBuilder();
+            wageBuilder.AddWageDetails("Walmart", 20, 21, 120);
+            wageBuilder.AddWageDetails("Dtech", 25, 27, 150);
+            wageBuilder.ComputeWageForAll();
+
         }
+
     }
 }
