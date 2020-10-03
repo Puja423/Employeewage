22eb501ccb4d90695056eb1605a5456e9a3bdaf7
Puja <pujayadav@gmail.com>
2020-10-03 13:45:52 +0530

    [Puja] Save a daily wage

---
+++ employeewage/Companywage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeesWageProgram
+{
+    class CompanyEmployeeWage
+    {
+        public string companyName;
+        public int wagePerHour;
+        public int workingDays;
+        public int maxHoursPerMonth;
+        public List<int> DailyWage;
+        public int totalEmployeeWage;
+
+        public CompanyEmployeeWage(string companyName, int wagePerHour, int workingDays, int maxHoursPerMonth)
+        {
+            this.companyName = companyName;
+            this.wagePerHour = wagePerHour;
+            this.workingDays = workingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.DailyWage = new List<int>();
+        }
+        public override string ToString()
+        {
+            return "Total Employee Wage for " + companyName + " is " + totalEmployeeWage;
+        }
+    }
+}
---
+++ employeewage/EmployeeWageBuilder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeesWageProgram
+{
+
+    class EmployeeWageBuilder : IEmployeeWageBuilder
+    {
+        public const int IS_Part_Time = 1;
+        public const int IS_Full_Time = 2;
+
+
+        private int _numberOfCompanies = 0;
+        public ArrayList companyWageArray;
+        public Dictionary<string, int> companyWageMap;
+
+
+        public EmployeeWageBuilder()
+        {
+            companyWageArray = new ArrayList();
+            companyWageMap = new Dictionary<string, int>();
+        }
+
+        public void AddWageDetails(string companyName, int wagePerHour, int workingDays, int maxHoursPerMonth)
+        {
+            companyWageArray.Add(new CompanyEmployeeWage(companyName, wagePerHour, workingDays, maxHoursPerMonth));
+            _numberOfCompanies++;
+        }
+
+        public void ComputeWageForAll()
+        {
+            for (int i = 0; i < _numberOfCompanies; i++)
+            {
+                var company = (CompanyEmployeeWage)companyWageArray[i];
+                CalculateMonthlyWage(company);
+                companyWageMap.Add(company.companyName, company.totalEmployeeWage);
+                //Console.WriteLine(companyWageArray[i].ToString());
+            }
+        }
+        public void CalculateMonthlyWage(CompanyEmployeeWage companyEmployeeWage)
+        {
+            var wageHour = 0;
+            var totalHours = 0;
+            var totalWorkingDays = 0;
+            var totalEmployeeWage = 0;
+            while (totalHours <= companyEmployeeWage.maxHoursPerMonth && totalWorkingDays <= companyEmployeeWage.workingDays)
+            {
+                totalWorkingDays++;
+                var empCheck = new Random().Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_Part_Time:
+                        wageHour = 4;
+                        break;
+                    case IS_Full_Time:
+                        wageHour = 8;
+                        break;
+                    default:
+                        wageHour = 0;
+                        break;
+                }
+                totalHours += wageHour;
+                var dailywage = wageHour * companyEmployeeWage.wagePerHour;
+                companyEmployeeWage.DailyWage.Add(dailywage);
+            }
+            totalEmployeeWage = totalHours * companyEmployeeWage.wagePerHour;
+            companyEmployeeWage.totalEmployeeWage = totalEmployeeWage;
+            //Console.WriteLine("Total Employee Wage for " + companyEmployeeWage.companyName + " is " + totalEmployeeWage);
+        }
+
+
+
+
+    }
+}
